be9fb10ced5f7c767343c1ed4eb65492ffe202a6
Eduardo <eduardomelle@gmail.com>
2022-03-01 15:03:47 -0300

    Added to the project: - openjdk 11 slim to create the Docker image; - Spring Retry to run with Docker Compose; - Open API (Swagger 3) to expose the contract with your documentation.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:11
+FROM openjdk:11-jdk-slim
 
 ADD target/department-service-0.0.1-SNAPSHOT.jar department-service.jar
 
--- src/main/java/com/dailycodebuffer/department/DepartmentServiceApplication.java
+++ src/main/java/com/dailycodebuffer/department/DepartmentServiceApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 @EnableEurekaClient
+@OpenAPIDefinition(info = @Info(title = "Departments API", version = "1.0", description = "Departments Information"))
 public class DepartmentServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentServiceApplication.class, args);
 	}
